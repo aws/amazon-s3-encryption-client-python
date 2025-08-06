52dc9ab7dd7986c75abf971f63494708fd0d5cd7
Kess Plasmeier <kessplas@amazon.com>
2025-08-06 15:54:11 -0700

    fix region

--- test/integration/test_i_s3_encryption.py
+++ test/integration/test_i_s3_encryption.py
@@ -7,6 +7,7 @@ from s3_encryption import S3EncryptionClient, S3EncryptionClientConfig
 from s3_encryption.materials.kms_keyring import KmsKeyring
 
 bucket = os.environ.get("CI_S3_BUCKET", "s3ec-python-github-test-bucket")
+region = os.environ.get("CI_AWS_REGION", "us-west-2")
 kms_key_id = os.environ.get("CI_KMS_KEY_ALIAS", "arn:aws:kms:us-west-2:370957321024:alias/S3EC-Python-Github-KMS-Key")
 
 def test_simple_roundtrip():
@@ -15,7 +16,7 @@ def test_simple_roundtrip():
 
     data = "test input for simple v3 round trip"
 
-    kms_client = boto3.client("kms", region_name="us-east-2")
+    kms_client = boto3.client("kms", region_name=region)
 
     keyring = KmsKeyring(kms_client, kms_key_id)
 
