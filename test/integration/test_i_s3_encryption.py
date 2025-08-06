99c9591f241cf4fd3b83f042472693218e9b557f
Kess Plasmeier <kessplas@amazon.com>
2025-08-06 15:42:19 -0700

    fix test

--- test/integration/test_i_s3_encryption.py
+++ test/integration/test_i_s3_encryption.py
@@ -1,11 +1,13 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 import boto3
+import os
 from datetime import datetime
 from s3_encryption import S3EncryptionClient, S3EncryptionClientConfig
 from s3_encryption.materials.kms_keyring import KmsKeyring
 
-bucket = "s3-ec-python-v3-test"
+bucket = os.environ.get("CI_S3_BUCKET", "s3ec-python-github-test-bucket")
+kms_key_id = os.environ.get("CI_KMS_KEY_ALIAS", "arn:aws:kms:us-west-2:370957321024:alias/S3EC-Python-Github-KMS-Key")
 
 def test_simple_roundtrip():
     key = "simple-rt"
@@ -13,7 +15,6 @@ def test_simple_roundtrip():
 
     data = "test input for simple v3 round trip"
 
-    kms_key_id = "arn:aws:kms:us-east-2:657301468084:key/1f469b1a-5cfa-4879-9bdf-27b3abd9b8d5"
     kms_client = boto3.client("kms", region_name="us-east-2")
 
     keyring = KmsKeyring(kms_client, kms_key_id)
