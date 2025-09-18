6255978d12e4f206157430c80476ee9a13cf2e39
rishav-karanjit <rishavkj@amazon.com>
2025-09-18 15:23:29 -0700

    Error model

--- test-server/net-v2-v3-server/Models/ErrorModels.cs
+++ test-server/net-v2-v3-server/Models/ErrorModels.cs
@@ -2,12 +2,12 @@ namespace NetV2V3Server.Models;
 
 public class GenericServerError
 {
-    public string __type { get; set; } = "software.amazon.encryption.s3#GenericServerError";
+    public string Type { get; set; } = "software.amazon.encryption.s3#GenericServerError";
     public string Message { get; set; } = string.Empty;
 }
 
 public class S3EncryptionClientError
 {
-    public string __type { get; set; } = "software.amazon.encryption.s3#S3EncryptionClientError";
+    public string Type { get; set; } = "software.amazon.encryption.s3#S3EncryptionClientError";
     public string Message { get; set; } = string.Empty;
 }
