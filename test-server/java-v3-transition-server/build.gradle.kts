1546f909b6bf9a1daf65bcfc8cd20b40c29483f5
Darwin Chowdary <39110935+imabhichow@users.noreply.github.com>
2025-09-29 08:42:44 -0700

    chore: add s3ec-java v3 (transition) test server

---
+++ test-server/java-v3-transition-server/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    `java-library`
+    id("software.amazon.smithy.gradle.smithy-base")
+    application
+}
+
+dependencies {
+    val smithyJavaVersion: String by project
+
+    smithyBuild("software.amazon.smithy.java:plugins:$smithyJavaVersion")
+
+    implementation("software.amazon.smithy:smithy-rules-engine:1.59.0")
+    implementation("software.amazon.smithy.java:server-netty:$smithyJavaVersion")
+    implementation("software.amazon.smithy.java:aws-server-restjson:$smithyJavaVersion")
+
+    // S3EC from local Maven repository (installed by mvn install)
+    implementation("software.amazon.encryption.s3:amazon-s3-encryption-client-java:3.4.0-SNAPSHOT")
+}
+
+// Use that application plugin to start the service via the `run` task.
+application {
+    mainClass = "software.amazon.encryption.s3.S3ECJavaTestServer"
+}
+
+// Add generated Java files to the main sourceSet
+afterEvaluate {
+    val serverPath = smithy.getPluginProjectionPath(smithy.sourceProjection.get(), "java-server-codegen")
+    sourceSets {
+        main {
+            java {
+                srcDir(serverPath)
+            }
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        dependsOn(smithyBuild)
+    }
+}
+
+// Helps Intellij IDE's discover smithy models
+sourceSets {
+    main {
+        java {
+            srcDir("../model")
+        }
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
