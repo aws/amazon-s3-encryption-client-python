7f31a7415f21e750a02c8a656f3960ca427b9223
rishav-karanjit <rishavkj@amazon.com>
2025-10-28 22:08:06 -0700

    auto commit

--- test-server/java-tests/src/it/java/software/amazon/encryption/s3/TestUtils.java
+++ test-server/java-tests/src/it/java/software/amazon/encryption/s3/TestUtils.java
@@ -68,8 +68,10 @@ public class TestUtils {
     public static final String GO_V4 = "Go-V4";
 
     public static final String NET_V2_CURRENT = "NET-V2-Current";
+    public static final String NET_V3_CURRENT = "NET-V3-Current";
     public static final String NET_V2_TRANSITION = "NET-V2-Transition";
-    public static final String NET_V3 = "NET-V3";
+    public static final String NET_V3_TRANSITION = "NET-V3-Transition";
+    public static final String NET_V4 = "NET-V4";
 
     public static final String CPP_V2_CURRENT = "CPP-V2-Current";
     public static final String CPP_V2_TRANSITION = "CPP-V2-Transition";
@@ -92,17 +94,18 @@ public class TestUtils {
 
     // Sets of unsupported features by language
     public static final Set<String> ENCRYPTION_CONTEXT_ON_DECRYPT_UNSUPPORTED =
-        Set.of(GO_V3_CURRENT, PHP_V2_CURRENT, PHP_V2_TRANSITION, PHP_V3, NET_V2_CURRENT, NET_V3);
+        Set.of(GO_V3_CURRENT, PHP_V2_CURRENT, PHP_V2_TRANSITION, PHP_V3, NET_V2_CURRENT, NET_V3_CURRENT, NET_V3_TRANSITION, NET_V4);
     
     public static final Set<String> ENCRYPTION_CONTEXT_ON_ENCRYPT_UNSUPPORTED =
-        Set.of(NET_V2_CURRENT, NET_V3);
+        Set.of(NET_V2_CURRENT, NET_V3_CURRENT, NET_V3_TRANSITION, NET_V4);
 
     public static final Set<String> CURRENT_VERSIONS =
         Set.of(
             JAVA_V3_CURRENT,
             GO_V3_CURRENT,
             NET_V2_CURRENT,
-            // CPP_V2_CURRENT,
+            NET_V3_CURRENT,
+            CPP_V2_CURRENT,
             RUBY_V2_CURRENT,
             PHP_V2_CURRENT
         );
@@ -112,7 +115,8 @@ public class TestUtils {
             // JAVA_V3_TRANSITION,
             // GO_V3_TRANSITION,
             // NET_V2_TRANSITION,
-            // CPP_V2_TRANSITION,
+            // NET_V3_TRANSITION,
+            CPP_V2_TRANSITION,
             // PHP_V2_TRANSITION,
             RUBY_V2_TRANSITION
         );
@@ -121,8 +125,8 @@ public class TestUtils {
         Set.of(
             // JAVA_V4,
             // PYTHON_V3,
-            // GO_V4,
-            // NET_V3,
+            GO_V4,
+            // NET_V4,
             // CPP_V3,
             // PHP_V3,
             RUBY_V3
@@ -136,21 +140,23 @@ public class TestUtils {
         servers.put(PYTHON_V3, new LanguageServerTarget(PYTHON_V3, "8081"));
         servers.put(GO_V3_CURRENT, new LanguageServerTarget(GO_V3_CURRENT, "8082"));
         servers.put(NET_V2_CURRENT, new LanguageServerTarget(NET_V2_CURRENT, "8083"));
-        servers.put(NET_V3, new LanguageServerTarget(NET_V3, "8084"));
-        // servers.put(CPP_V2_CURRENT, new LanguageServerTarget(CPP_V2_CURRENT, "8085"));
+        servers.put(NET_V3_CURRENT, new LanguageServerTarget(NET_V3_CURRENT, "8084"));
+        servers.put(CPP_V2_CURRENT, new LanguageServerTarget(CPP_V2_CURRENT, "8085"));
         // servers.put(RUBY_V2_CURRENT, new LanguageServerTarget(RUBY_V2_CURRENT, "8086"));
         servers.put(PHP_V2_CURRENT, new LanguageServerTarget(PHP_V2_CURRENT, "8087"));
+        servers.put(JAVA_V4, new LanguageServerTarget(JAVA_V4, "8088"));
         servers.put(GO_V4, new LanguageServerTarget(GO_V4, "8089"));
+        servers.put(NET_V4, new LanguageServerTarget(NET_V4, "8090"));
         servers.put(RUBY_V3, new LanguageServerTarget(RUBY_V3, "8092"));
         servers.put(PHP_V3, new LanguageServerTarget(PHP_V3, "8093"));
         // TODO: Create and add transition servers
         servers.put(JAVA_V3_TRANSITION, new LanguageServerTarget(JAVA_V3_TRANSITION, "8094"));
         // servers.put(GO_V3_TRANSITION, new LanguageServerTarget(GO_V3_TRANSITION, "8095"));
         // servers.put(NET_V2_TRANSITION, new LanguageServerTarget(NET_V2_TRANSITION, "8096"));
-        // servers.put(CPP_V2_TRANSITION, new LanguageServerTarget(CPP_V2_TRANSITION, "8097"));
+        servers.put(CPP_V2_TRANSITION, new LanguageServerTarget(CPP_V2_TRANSITION, "8097"));
         servers.put(RUBY_V2_TRANSITION, new LanguageServerTarget(RUBY_V2_TRANSITION, "8098"));
         servers.put(PHP_V2_TRANSITION, new LanguageServerTarget(PHP_V2_TRANSITION, "8099"));
-        servers.put(JAVA_V4, new LanguageServerTarget(JAVA_V4, "8090"));
+        // servers.put(NET_V3_TRANSITION, new LanguageServerTarget(NET_V3_TRANSITION, "8100"));
         serverMap = filterServers(servers);
     }
 
