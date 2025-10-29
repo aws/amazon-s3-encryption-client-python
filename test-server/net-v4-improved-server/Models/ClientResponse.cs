32bfe1b6986dfe01d04da0156025c5e52ff56b50
rishav-karanjit <rishavkj@amazon.com>
2025-10-28 17:43:10 -0700

    Deleted wrong dir

--- test-server/net-v4-improved-server/Models/ClientResponse.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace NetV4ImprovedServer.Models;
-
-public class ClientResponse
-{
-    [JsonPropertyName("clientId")] public string ClientId { get; set; } = string.Empty;
-}
