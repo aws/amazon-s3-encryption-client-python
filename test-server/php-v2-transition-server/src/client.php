85d6bb553ac9e8b6f09444b2fbba2a79a150f7a9
Jose Corella <jocorell@amazon.com>
2025-10-30 15:24:43 -0700

    test server updates

--- test-server/php-v2-transition-server/src/client.php
+++ test-server/php-v2-transition-server/src/client.php
@@ -19,12 +19,19 @@ function handleCreateClient()
     $legacyAlgorithms = $configData["enableLegacyWrappingAlgorithms"] ?? false;
     $clientId = Uuid::uuid4()->toString();
     $kmsKeyId = $keyMaterial["kmsKeyId"] ?? null;
+    $commitmentPolicy = $configData['commitmentPolicy'] ?? "FORBID_ENCRYPT_ALLOW_DECRYPT";
 
     if ($configData == []) {
         return GenericServerError("Invalid config in request body", 400);
     }
     if (($keyMaterial || $kmsKeyId) === null) {
         return GenericServerError("Invalid keyMaterial in config", 400);
+    }
+    if ($commitmentPolicy !== "FORBID_ENCRYPT_ALLOW_DECRYPT") {
+        return GenericServerError(
+            "Transition server only supports FORBID_ENCRYPT_ALLOW_DECRYPT"
+            . "commitment policy but received {$commitmentPolicy}"
+        );
     }
 
     // Store client configuration instead of objects (AWS objects can't be serialized)
@@ -55,6 +62,7 @@ function handleCreateClient()
         ],
         'kmsKeyId' => $kmsKeyId,
         'legacy' => $legacyAlgorithms,
+        'commitmentPolicy' => $commitmentPolicy,
         'created' => time()
     ];
 
