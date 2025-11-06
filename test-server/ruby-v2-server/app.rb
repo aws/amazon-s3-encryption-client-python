7bbf3789549e03f92e32eb57114dafad70028ada
Ryan Emery <ryanemer@amazon.com>
2025-11-06 15:42:30 -0800

    try this?

--- test-server/ruby-v2-server/app.rb
+++ test-server/ruby-v2-server/app.rb
@@ -5,6 +5,8 @@ require_relative 'lib/metadata_utils'
 require_relative 'lib/error_handlers'
 require_relative 'lib/logger'
 
+Aws.use_bundled_cert!
+
 class S3ECRubyServer < Sinatra::Base
   configure do
     set :port, ENV['PORT'] || 8098
