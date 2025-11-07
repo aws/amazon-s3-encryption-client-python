9acf8ad41fe557d4303f2843a2b64f14d195162c
Darwin Chowdary <39110935+imabhichow@users.noreply.github.com>
2025-11-07 12:34:04 -0800

    Change java-v4-port

--- test-server/java-tests/src/it/java/software/amazon/encryption/s3/TestUtils.java
+++ test-server/java-tests/src/it/java/software/amazon/encryption/s3/TestUtils.java
@@ -171,7 +171,7 @@ public class TestUtils {
         // servers.put(NET_V2_TRANSITION, new LanguageServerTarget(NET_V2_TRANSITION, "8096"));
         // servers.put(RUBY_V2_TRANSITION, new LanguageServerTarget(RUBY_V2_TRANSITION, "8098"));
         servers.put(PHP_V2_TRANSITION, new LanguageServerTarget(PHP_V2_TRANSITION, "8099"));
-        servers.put(JAVA_V4, new LanguageServerTarget(JAVA_V4, "8090"));
+        servers.put(JAVA_V4, new LanguageServerTarget(JAVA_V4, "8088"));
         servers.put(NET_V3_TRANSITION, new LanguageServerTarget(NET_V3_TRANSITION, "8100"));
         serverMap = filterServers(servers);
     }
