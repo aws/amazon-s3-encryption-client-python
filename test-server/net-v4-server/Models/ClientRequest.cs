473ce034ee7f8c0df02353aaeef20977d9ea3087
rishav-karanjit <rishavkj@amazon.com>
2025-11-11 23:45:49 -0800

    auto commit

--- test-server/net-v4-server/Models/ClientRequest.cs
+++ test-server/net-v4-server/Models/ClientRequest.cs
@@ -27,9 +27,7 @@ public class KeyMaterial
 {
     public byte[]? RsaKey { get; set; }
     public byte[]? AesKey { get; set; }
-
-    [Required]
-    public string KmsKeyId { get; set; } = string.Empty;
+    public string? KmsKeyId { get; set; }
 }
 
 [JsonConverter(typeof(JsonStringEnumConverter))]
