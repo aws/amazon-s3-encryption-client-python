ab14133f276712d42290a24e46e214c9c3fc1889
Ryan Emery <ryanemer@amazon.com>
2025-11-18 15:12:38 -0800

    fix for Java?

--- test-server/java-tests/build.gradle.kts
+++ test-server/java-tests/build.gradle.kts
@@ -61,8 +61,8 @@ tasks {
         systemProperty("junit.jupiter.execution.parallel.config.strategy", "fixed")
         maxParallelForks = 1  // One JVM
         systemProperty("junit.jupiter.execution.parallel.config.fixed.parallelism", 
-            Runtime.getRuntime().availableProcessors().toString())  // Scale with CPU
-        
+            Math.max(1, Runtime.getRuntime().availableProcessors() - 2).toString())  // Scale with CPU, reserve 2 cores
+
         // Passing information from Gradle into the tests so that we can filter our servers
         systemProperty("test.filter.servers", System.getProperty("test.filter.servers"))
         // For debugging
