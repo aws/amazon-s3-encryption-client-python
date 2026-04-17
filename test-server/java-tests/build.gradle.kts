40f8aff9cd7ea6c378f12bbcf1eb59b6cb2c6b4a
Kess Plasmeier <kessplas@amazon.com>
2026-04-17 16:47:20 -0700

    remove stray comment

--- test-server/java-tests/build.gradle.kts
+++ test-server/java-tests/build.gradle.kts
@@ -66,6 +66,7 @@ tasks {
         // Passing information from Gradle into the tests so that we can filter our servers
         systemProperty("test.filter.servers", System.getProperty("test.filter.servers"))
         // For debugging
+        // // Enable System.out output
         // testLogging {
         //     events("passed", "skipped", "failed", "standardOut", "standardError")
         //     showStandardStreams = true
