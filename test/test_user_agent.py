89940c9e2cec9e6f64153b5884aa65d0535450f0
Kess Plasmeier <kessplas@amazon.com>
2026-05-12 16:06:45 -0700

    linter again ole ole

--- test/test_user_agent.py
+++ test/test_user_agent.py
@@ -1,6 +1,7 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Unit tests for user agent string injection."""
+
 import boto3
 
 from s3_encryption import S3EncryptionClient, S3EncryptionClientConfig
