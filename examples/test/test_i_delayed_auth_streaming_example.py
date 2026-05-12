78a69131a452dc7db9df15f7d442e658311109ba
Kess Plasmeier <kessplas@amazon.com>
2026-05-12 15:21:51 -0700

    add uuid to examples, use 3.10

--- examples/test/test_i_delayed_auth_streaming_example.py
+++ examples/test/test_i_delayed_auth_streaming_example.py
@@ -1,6 +1,9 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Test suite for the delayed auth streaming decrypt example."""
+
+import uuid
+
 import boto3
 import pytest
 
@@ -9,11 +12,11 @@ from ..src.delayed_auth_streaming_example import delayed_auth_streaming_decrypt
 pytestmark = [pytest.mark.examples]
 
 BUCKET = "s3ec-python-github-test-bucket"
-KEY = "examples/delayed-auth-streaming"
 KMS_KEY_ID = "arn:aws:kms:us-west-2:370957321024:alias/S3EC-Python-Github-KMS-Key"
 
 
 def test_delayed_auth_streaming_decrypt():
+    key = f"examples/delayed-auth-streaming-{uuid.uuid4()}"
     s3_client = boto3.client("s3", region_name="us-west-2")
     kms_client = boto3.client("kms", region_name="us-west-2")
     delayed_auth_streaming_decrypt(
@@ -21,7 +24,6 @@ def test_delayed_auth_streaming_decrypt():
         kms_client=kms_client,
         kms_key_id=KMS_KEY_ID,
         bucket=BUCKET,
-        key=KEY,
+        key=key,
     )
-    # Clean up
-    s3_client.delete_object(Bucket=BUCKET, Key=KEY)
+    s3_client.delete_object(Bucket=BUCKET, Key=key)
--- examples/test/test_i_kms_keyring_put_get_example.py
+++ examples/test/test_i_kms_keyring_put_get_example.py
@@ -1,6 +1,9 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Test suite for the KMS Keyring put/get example."""
+
+import uuid
+
 import boto3
 import pytest
 
@@ -9,11 +12,11 @@ from ..src.kms_keyring_put_get_example import kms_keyring_put_get
 pytestmark = [pytest.mark.examples]
 
 BUCKET = "s3ec-python-github-test-bucket"
-KEY = "examples/kms-keyring-put-get"
 KMS_KEY_ID = "arn:aws:kms:us-west-2:370957321024:alias/S3EC-Python-Github-KMS-Key"
 
 
 def test_kms_keyring_put_get():
+    key = f"examples/kms-keyring-put-get-{uuid.uuid4()}"
     s3_client = boto3.client("s3", region_name="us-west-2")
     kms_client = boto3.client("kms", region_name="us-west-2")
     kms_keyring_put_get(
@@ -21,7 +24,6 @@ def test_kms_keyring_put_get():
         kms_client=kms_client,
         kms_key_id=KMS_KEY_ID,
         bucket=BUCKET,
-        key=KEY,
+        key=key,
     )
-    # Clean up
-    s3_client.delete_object(Bucket=BUCKET, Key=KEY)
+    s3_client.delete_object(Bucket=BUCKET, Key=key)
