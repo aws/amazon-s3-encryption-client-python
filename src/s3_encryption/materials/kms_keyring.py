ac493e5157afead45b563a0323b9dffb5d46ccef
Anirav Kareddy <aniravk@amazon.com>
2026-09-17 22:30:53 -0700

    simplified logic in setting the encryption context algorithm value

--- src/s3_encryption/materials/kms_keyring.py
+++ src/s3_encryption/materials/kms_keyring.py
@@ -10,7 +10,6 @@ from attrs import define, field
 from botocore import client
 
 from ..exceptions import S3EncryptionClientError
-from ..materials.materials import AlgorithmSuite
 from .encrypted_data_key import EncryptedDataKey
 from .keyring import S3Keyring
 
@@ -84,18 +83,10 @@ class KmsKeyring(S3Keyring):
             ##% The KmsKeyring MUST NOT support encryption using KmsV1 mode.
             # For committing algorithm suites (V3), the encryption context algorithm
             # value is the algorithm suite ID as a string ("115"), not the cipher name.
-            # For non-committing suites (V2), use the cipher name ("AES/GCM/NoPadding").
-            if (
-                enc_materials.encryption_algorithm
-                == AlgorithmSuite.ALG_AES_256_GCM_HKDF_SHA512_COMMIT_KEY
-            ):
-                encryption_context["aws:x-amz-cek-alg"] = str(
-                    enc_materials.encryption_algorithm.suite_id
-                )
-            else:
-                encryption_context["aws:x-amz-cek-alg"] = (
-                    enc_materials.encryption_algorithm.cipher_name
-                )
+            # For non-committing suites (V2), it is the cipher name ("AES/GCM/NoPadding").
+            encryption_context["aws:x-amz-cek-alg"] = (
+                enc_materials.encryption_algorithm.content_cipher
+            )
 
             # Python implementation uses KMS GenerateDataKey instead of the spec's
             # EncryptDataKey pattern
